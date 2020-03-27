b665f10a35fc05e1a24b98784e2b6a480c786fb4
Sleepk <julio.cesar.vgs@gmail.com>
2020-03-27 01:47:52 -0300

    Projeto rodando com JWT

---
+++ Shop/Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Services
+{
+    public static class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var direitos = new[]
+             {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Username.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                    //new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    //new Claim(ClaimTypes.Role, user.Role.ToString())
+             };
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.Secret));
+            //gerado a credenciais para gerar o token
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: "Shop.WebApp",
+                audience: "Postman",
+                claims: direitos,
+                signingCredentials: credenciais,
+                expires: DateTime.Now.AddMinutes(30)
+                );
+
+            var validacaoToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return validacaoToken;
+        }
+    }
+}
---
+++ Shop/Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Shop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options =>
+            {
+                //objeto para validação do token
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    //Quem valida
+                    ValidateIssuer = true,
+                    //Quem esta pedindo o token
+                    ValidateAudience = true,
+                    //validador o tempo da requisicao
+                    ValidateLifetime = true,
+                   //validar a chave
+                    ValidateIssuerSigningKey = true,
+                    //string que vai validar no juiz
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.Secret)),
+                    // tempo de expiração
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    ValidIssuer = "Shop.WebApp",
+                    ValidAudience = "Postman",
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
